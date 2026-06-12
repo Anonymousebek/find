976a74fba309e44e75172e798955f06696258e61
Anonymousebek <anonymousebek@gmail.com>
2026-06-12 19:34:48 +0600

    Delete duplicate test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -63,14 +63,6 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal [ I18n.translate("errors.messages.taken") ], product.errors[:title]
   end
 
-  test "should validate product title minimum length" do
-    product = Product.new(title: "T", description: products(:dune).title, price: 1)
-    product.image.attach(io: File.open("test/fixtures/files/dune.jpg"), filename: "dune.jpg", content_type: "image/jpeg")
-
-    assert product.invalid?
-    assert product.errors["title"].any?
-  end
-
   test "should fail when product title is too short" do
     product = Product.new(title: "T", description: products(:dune).title, price: 1)
     product.image.attach(io: File.open("test/fixtures/files/dune.jpg"), filename: "dune.jpg", content_type: "image/jpeg")
